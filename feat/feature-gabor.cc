46e512222f90cb5d254dcbfbc3b5254221682b10
Puneet Bawa <bawapuneet994@gmail.com>
2021-04-15 09:27:13 +0530

    Update feature-gabor.cc

--- feat/feature-gabor.cc
+++ feat/feature-gabor.cc
@@ -1,21 +1,5 @@
 // feat/feature-gabor.cc
 
-// Copyright 2014  Jimmy & Danny
-// July 2014: modified by Maarten Van Segbroeck 
-
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//  http://www.apache.org/licenses/LICENSE-2.0
-//
-// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
-// WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
-// MERCHANTABLITY OR NON-INFRINGEMENT.
-// See the Apache 2 License for the specific language governing permissions and
-// limitations under the License.
-
 #include "feat/feature-gabor.h"
 #include "time.h"
 
